695689e325cbd0d7f9e2105a3112544206798f27
stas <stas@morda.(none)>
2016-09-30 00:54:44 +0400

    fix

--- src/fastnetmon.cpp
+++ src/fastnetmon.cpp
@@ -4340,7 +4340,6 @@ bool we_should_ban_this_ip(map_element* average_speed_element, ban_settings_t cu
     bool attack_detected_by_bandwidth = false;
     bool attack_detected_by_flow = false;
 
-    logger << log4cpp::Priority::INFO  << "---- " << current_ban_settings.c_host_group_name << current_ban_settings.enable_custom_ban_for_all_unit;
     if (current_ban_settings.enable_custom_ban_for_all_unit && load_signature_file) {
         int signature_count;
         signature_count = array_list_length(json_object_get_array(json_object_object_get(signature_jobj, "signature")));
@@ -4353,9 +4352,10 @@ bool we_should_ban_this_ip(map_element* average_speed_element, ban_settings_t cu
             int params_count = 0;
             int params_inc = 0;
             
-            /*if (current_ban_settings.host_group_name == json_object_to_json_string(json_object_object_get(signature, "group"))) {
+            logger << log4cpp::Priority::INFO  << "---- " << current_ban_settings.c_host_group_name << current_ban_settings.enable_custom_ban_for_all_unit;
+            if ("\""+current_ban_settings.host_group_name+"\"" == json_object_to_json_string(json_object_object_get(signature, "group"))) {
                 logger << log4cpp::Priority::INFO  << "---- CCCURRENT BAN SETTINGS (HOST_GROUP_NAME): " << current_ban_settings.host_group_name;
-            }*/
+            }
 
             if (strcmp(json_object_to_json_string(json_object_object_get(signature, "protocol")),"\"tcp\"") == 0) {
                 // check TCP flags                
